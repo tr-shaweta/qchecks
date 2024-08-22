ae65dc3b21180d6d13708202f688edf2a853463f
Shaweta <choudharyshaweta@gmail.com>
2024-08-22 13:24:04 +0530

    Q changes

--- qualitychecks/settings.py
+++ qualitychecks/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Qualitychecks.urls'
+ROOT_URLCONF = 'qualitychecks.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Qualitychecks.wsgi.application'
+WSGI_APPLICATION = 'qualitychecks.wsgi.application'
 
 
 # Database
--- qualitychecks/wsgi.py
+++ qualitychecks/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Qualitychecks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qualitychecks.settings')
 
 application = get_wsgi_application()
 app=application
